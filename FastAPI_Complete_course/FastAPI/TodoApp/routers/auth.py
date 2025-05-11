6dbe12bd0b23608f74cae9257226a1eae2f331db
Cris <cristianarellanoagudo@gmail.com>
2025-05-11 16:58:33 +0200

    feat(auth): enhance authentication with error handling and current user retrieval and decode JWT

--- FastAPI_Complete_course/FastAPI/TodoApp/routers/auth.py
+++ FastAPI_Complete_course/FastAPI/TodoApp/routers/auth.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from typing import Annotated
 from pydantic import BaseModel
 from models import Users
@@ -7,16 +7,20 @@ from passlib.context import CryptContext
 from database import SessionLocal
 from sqlalchemy.orm import Session
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
 
 SECRET_KEY = "1eda56e2b116bf90db4528c73977b88491463e4e81c54872b7df34ea87655db8"
 ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-        
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+
 class CreateUserRequest(BaseModel):
     username: str
     email: str
@@ -54,7 +58,18 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     
     create_user_request = Users(
@@ -75,7 +90,7 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
-        return {"error": "Invalid credentials"}
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
     token = create_access_token(
         user.username,
         user.id,
