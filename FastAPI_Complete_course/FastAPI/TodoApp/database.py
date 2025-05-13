d871d81a8cec5a8b80d06c8845a50c7b6bc4f77a
Cris <cristianarellanoagudo@gmail.com>
2025-05-13 20:13:57 +0200

    feat(database): update database configuration to use PostgreSQL

--- FastAPI_Complete_course/FastAPI/TodoApp/database.py
+++ FastAPI_Complete_course/FastAPI/TodoApp/database.py
@@ -2,10 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+#! PostgreSQL
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Criss027_@localhost/TodoApplicationDatabase'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+#! PostgreSQL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
